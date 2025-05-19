adc7c7bc8c36995153344928196b9d56c9d01633
Salman <salzahir18@gmail.com>
2025-05-18 23:10:03 -0500

    fix: update server paths and scripts for new directory structure

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import postRoutes from './src/routes/post-routes';
 import authRoutes from './src/routes/auth-routes';
 import commentRoutes from './src/routes/comment-routes';
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 3000;
 const app = express();
 
