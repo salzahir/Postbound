236508ba5d3220d82cabca3b5949968ff3d35d84
Salman <salzahir18@gmail.com>
2025-05-18 22:03:36 -0500

    feat: add toggle public/private route

--- src/routes/post-routes.ts
+++ src/routes/post-routes.ts
@@ -5,8 +5,10 @@ import { handleIsAuthor } from "../controllers/auth-controller";
 import { authenticateToken } from "../middleware/jwt";
 
 router.get("/", postController.handleGetPosts);
+
 router.post("/", authenticateToken, handleIsAuthor, postController.handlePostPost);
 router.put("/:id", authenticateToken, handleIsAuthor, postController.handleUpdatePost);
 router.delete("/:id", authenticateToken, handleIsAuthor, postController.handleDeletePost);
+router.patch("/:id/toggle", authenticateToken, handleIsAuthor, postController.handleTogglePost);
 
 export default router;
