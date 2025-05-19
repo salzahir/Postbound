f557beeeca5b21b5ef7094c37f3a63a8c32ad0e2
Salman <salzahir18@gmail.com>
2025-05-18 22:37:44 -0500

    refactor: simplify auth route validation to match other routes

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,15 +1,11 @@
 import * as authController from '../controllers/auth-controller';
-import { Router, RequestHandler } from 'express';
+import { Router } from 'express';
 import { validForm, handleErrors } from '../middleware/error';
 
 const router = Router();
 
 router.get("/users", authController.handleGetUsers);
-router.post(
-  "/sign-up",
-  [...validForm, handleErrors] as RequestHandler[],
-  authController.handlePostUser
-);
+router.post("/sign-up", validForm, handleErrors, authController.handlePostUser);
 router.post("/login", authController.handleGetLogin);
 
 export default router;
