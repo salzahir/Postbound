38145dac4ff1e1f672890c7854904cc33eff0c60
Salman <salzahir18@gmail.com>
2025-05-24 18:56:56 -0500

    feat: add comment types for frontend

---
+++ client/src/types/comments.ts
@@ -0,0 +1,11 @@
+interface Comment {
+    id: number;
+    title: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    userId: string;
+    postId: number;
+}
+
+export type { Comment };
