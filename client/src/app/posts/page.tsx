230f4db1c5a31b591d38c360d7c603ea624a451d
Salman <salzahir18@gmail.com>
2025-05-24 18:13:24 -0500

    refactor: update posts page to support authentication tokens for future comment functionality

--- client/src/app/posts/page.tsx
+++ client/src/app/posts/page.tsx
@@ -1,17 +1,46 @@
+"use client";
 import Header from "../header";
 import PostCard from "./PostCard";
 import { Post } from "@/types/posts";
+import { useEffect, useState } from "react";
 
-async function Posts() {
-    const posts = await fetch("http://localhost:3001/posts");
-    const data = await posts.json() as Post[];
+function Posts() {
     
-    if(data.length === 0) {
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const[token, setToken] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const storedToken = localStorage.getItem("token");
+                if (storedToken) {
+                    setToken(storedToken);
+                }
+                const fetchedPosts = await fetch("http://localhost:3001/posts");
+                const data = await fetchedPosts.json() as Post[];
+                setPosts(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+                setError("Failed to fetch posts");
+                setLoading(false);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+
+
+    if(posts.length === 0) {
         return (
             <>
                 <Header />
                 <div>
                     <h1>No posts available</h1>
+                    {loading && <p>Loading...</p>}
+                    {error && <p>{error}</p>}
                 </div>
             </>
         );
@@ -21,8 +50,9 @@ async function Posts() {
             <Header />
             <main>
                 <h1>Posts</h1>
+                <p>{token ? "You are logged in" : "You are not logged in"}</p>
                 <div>
-                    {data.map((post) => (
+                    {posts.map((post) => (
                           <PostCard key={post.id} post={post} />
                     ))}
                 </div>
